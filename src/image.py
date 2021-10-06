5042ccb50dc272151d82f881fde1ffc866680440
PyNvimDev <86599386+pynvimdev@users.noreply.github.com>
2021-10-06 16:24:24 +0530

    Add files via upload

---
+++ src/image.py
@@ -0,0 +1,9 @@
+from tkinter import PhotoImage
+startingimage2 = PhotoImage(file="../images/Start.png")
+nextimage = PhotoImage(file="../images/Next.png")
+previmage = PhotoImage(file="../images/Prev.png")
+startingimage = PhotoImage(file="../images/transparentGradHat.png")
+northstartbutton = PhotoImage(file="../images/North.png")
+astra = PhotoImage(file="../images/astra.png")
+Ruun = PhotoImage(file="../images/Run.png")
+quitimage = PhotoImage(file="../images/Quit.png")
